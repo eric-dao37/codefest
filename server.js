94751603320471e8e2fac2266ab6cc3b4cb0908e
Eric Dao <eric.dao@later.com>
2023-04-19 21:38:38 -0400

    Add 2 new endpoints

--- server.js
+++ server.js
@@ -19,37 +19,89 @@ app.listen(8080, () => {
   console.log("server started");
 });
 
+/**
+ * Endpoint to create image from caption 
+*/
 app.post("/create_image", async (req, res) => {
     const { caption } = req.body;
     try {
-      const response = await openai.createImage({
-        prompt: caption,
-        n: 1,
-        size: "1024x1024",
-      });
-      res.json({ url: response.data.data[0].url});
+        const response = await openai.createImage({
+            prompt: caption,
+            n: 1,
+            size: "1024x1024",
+        });
+        return res.status(200).json({
+            success: true,
+            url: response.data.data[0].url,
+        });
     } catch (err) {
-      res.send(err.message);
-      console.log(err.message);
+        res.send(err.message);
+        console.log(err.message);
+    }
+});
+
+/**
+ * End point to create caption
+ */
+app.post("/create_caption", async (req, res) => {
+    const { prompt } = req.body;
+    try {
+        if (prompt == null) {
+            throw new Error("Uh oh, no prompt was provided");
+        }
+
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt,
+        });
+
+        const completion = response.data.choices[0].text;
+
+        return res.status(200).json({
+            success: true,
+            message: completion,
+        });
+    } catch (err) {
+        res.send(err.message);
+        console.log(err.message);
 
     }
-  });
+});
 
-//   app.post("/create_caption", async (req, res) => {
-//     const { topic } = req.body;
-//     try {
-//       const response = await openai.create({
-//         prompt: caption,
-//         n: 1,
-//         size: "1024x1024",
-//       });
-//       res.send(response.data.data[0].url);
-//     } catch (err) {
-//       res.send(err.message);
-//       console.log(err.message);
+/**
+ * End point to create caption and image
+ */
+app.post("/create_all", async (req, res) => {
+    const { prompt } = req.body;
+    try {
+        if (prompt == null) {
+            throw new Error("Uh oh, no prompt was provided");
+        }
 
-//     }
-//   });
+        const responseCaption = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt,
+        });
+
+        console.log(responseCaption.data)
+
+        const completion = responseCaption.data.choices[0].text;
+
+        const responseImage = await openai.createImage({
+            prompt: completion,
+            n: 1,
+            size: "1024x1024",
+        });
+        return res.status(200).json({
+            success: true,
+            url: responseImage.data.data[0].url,
+            caption: completion,
+        });
+    } catch (err) {
+        res.send(err.message);
+        console.log(err.message);
+    }
+});
 
 app.get("/", async (req, res) => {
 
